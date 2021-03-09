b1e80b392d8ada36093c0f51e9944d106cdbee56
Maruf Ahmed Tushar <53092408+marufahmedtushar@users.noreply.github.com>
2021-03-09 20:07:51 +0600

    updated on 1-march-2021
    updated on 1-march-2021

--- blog/resources/views/index.blade.php
+++ blog/resources/views/index.blade.php
@@ -18,8 +18,8 @@
           <div class="item active">
             <div class="col-sm-6">
               <h1><span>E</span>-SHOPPER</h1>
-              <h2>Free E-Commerce Template</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              <h2></h2>
+              <p> </p>
               <button type="button" class="btn btn-default get">Get it now</button>
             </div>
             <div class="col-sm-6">
@@ -30,8 +30,8 @@
           <div class="item">
             <div class="col-sm-6">
               <h1><span>E</span>-SHOPPER</h1>
-              <h2>100% Responsive Design</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              <h2></h2>
+              <p> </p>
               <button type="button" class="btn btn-default get">Get it now</button>
             </div>
             <div class="col-sm-6">
@@ -43,8 +43,8 @@
           <div class="item">
             <div class="col-sm-6">
               <h1><span>E</span>-SHOPPER</h1>
-              <h2>Free Ecommerce Template</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+              <h2></h2>
+              <p> </p>
               <button type="button" class="btn btn-default get">Get it now</button>
             </div>
             <div class="col-sm-6">
